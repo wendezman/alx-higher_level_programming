c5c0960235c4f9656dc4c7f3127a75ce47322ba9
wendezman <wendezman@gmail.com>
2022-09-13 18:17:37 -0700

    python classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+""" Square module """
+
+
 class Square:
+    """ Declares a square class """
+
     def __init__(self, size=0) -> None:
+        """
+        Intializes the attributes
+        Args:
+            size: size of square
+        """
         self.size = size
 
     @property
     def size(self):
+        """ Gets the attribute to be used in class """
         return self.__size
 
     @size.setter
@@ -17,9 +28,11 @@ class Square:
             self.__size = value
 
     def area(self):
+        """ Computes area of a square """
         return self.__size ** 2
 
     def my_print(self):
+        """ Prints in stdout the square with the character # """
         if self.__size == 0:
             print()
         else:
